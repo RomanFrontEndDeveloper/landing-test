b89ab51178c6a27e551c8fd19e7794ade91269d3
RomanFronrEnd <romariotraveler@gmail.com>
2026-05-03 13:44:06 +0300

    add hero section with title, subtitle, CTA and image

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useState } from 'react';
+
+export const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<header className='w-full'>
+			<div className='max-w-[1280px] mx-auto px-[80px] md:px-6 lg:px-8 flex justify-between h-[164px] items-center'>
+				<div className='text-[20px] leading-[30px] font-medium'>
+					Site name
+				</div>
+
+				<nav className='hidden md:flex items-center gap-8 h-[52px]'>
+					<a
+						href='#'
+						className='text-[20px] leading-[30px] font-medium'
+					>
+						Page
+					</a>
+					<a
+						href='#'
+						className='text-[20px] leading-[30px] font-medium'
+					>
+						Page
+					</a>
+					<a
+						href='#'
+						className='text-[20px] leading-[30px] font-medium'
+					>
+						Page
+					</a>
+
+					<button className='h-[52px] px-6 bg-black text-white rounded-md'>
+						Send form
+					</button>
+				</nav>
+				<button
+					className='md:hidden flex items-center justify-center w-[54px] h-[30px]'
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					<img
+						src='/buttt.png'
+						alt='menu'
+						className='w-[51px] h-auto object-contain'
+					/>
+				</button>
+			</div>
+		</header>
+	);
+};
